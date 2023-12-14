3b2a92c698604cb7df93fed95535e25e8219c961
Thibault GARCIA-MEGEVAND <81759574+ThibaultGM78@users.noreply.github.com>
2023-12-14 14:30:24 +0100

    Update determinist.py

--- determinist.py
+++ determinist.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from automaton_q1 import *
 from debut import * 
 
 def is_automaton_deterministic(automaton):
@@ -24,7 +25,50 @@ def is_automaton_deterministic(automaton):
                     break    
                 else:
                     listTransi.append(transi[2])
+               
+def to_automaton_deterministic(afn):
+    afd = {
+        'alphabet': afn['alphabet'],
+        'states': [],
+        'initial_state': '',
+        'final_states': [],
+        'transitions': []
+    }
+    afn_states = sorted(afn['states'])
+    afd['initial_state'] = afn['initial_state']
+    afd['states'].append(afd['initial_state'])
+    queue = [afd['initial_state']]  
+    created_states = set(afd['initial_state'])
+    while queue:
+        current_afd_state = queue.pop(0)
+        current_afn_states = current_afd_state.split(', ')    
+        for letter in afn['alphabet']:
+            possible_states = set()
+            
+            for afn_state in current_afn_states:
+                for t in afn['transitions'] :
+                    if t[0] == afn_state and t[2] == letter :
+                        transitions = t[1]
+                        possible_states.update(transitions)
+            
+            possible_states = sorted(possible_states)
+            new_afd_state = ', '.join(possible_states) or 'null'                                
+            if new_afd_state not in afd['states'] and new_afd_state != 'null' and new_afd_state not in created_states:
+                queue.append(new_afd_state)
+                created_states.add(new_afd_state)
+                afd['states'].append(new_afd_state)
+                
+            for state in possible_states:
+                if state in afn['final_states']:
+                    afd['final_states'].append(new_afd_state)
+            if new_afd_state != 'null':
+                afd['transitions'].append([current_afd_state, new_afd_state, letter])
+        afd['final_states'] = sorted(set(afd['final_states']))
+    afd['transitions'] = sorted(afd['transitions'])
+    
+    return afd
 
+#print(to_automaton_deterministic(automate))
 
         
 #print(is_automaton_deterministic(automate))
