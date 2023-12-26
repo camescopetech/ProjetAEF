dd5ae5b4e85ff745e57c1eaa373f8e9cd44f1d00
CBezet <camille@la-forge.ai>
2023-12-26 11:03:29 +0100

    excise automaton

--- excise.py
+++ excise.py
@@ -1,73 +1,96 @@
-import automaton_q1 as q1
-import pandas as pd
-import re
-import random
-import time
+def epsilon_closure(state, transitions):
+    # Return all the state accessible from a given one, following only a given transition
+    closure = set([state])
+    stack = [state]
+
+    while stack:
+        current_state = stack.pop()
+        # Going all over the transitions to find the states accessible by this epsilon transition
+        for transition in transitions:
+            if transition[0] == current_state and transition[1] == '':
+                next_state = transition[2]
+                if next_state not in closure:
+                    closure.add(next_state)
+                    stack.append(next_state)
+
+    return closure
+
+def reachable_states(initial_state, transitions):
+    # Return all states accessible from the initial state
+    reachable = set([initial_state])
+    stack = [initial_state]
+
+    while stack:
+        current_state = stack.pop()
+        # Going over the transitions to find the accessible states
+        for transition in transitions:
+            if transition[0] == current_state:
+                next_state = transition[2]
+                if next_state not in reachable:
+                    reachable.add(next_state)
+                    stack.append(next_state)
+
+    return reachable
+
+def epsilon_closure_of_set(states, transitions):
+    # Return the set of all states reachable from any state in the automaton
+    # by following epsilon transitions
+    closure = set()
+    for state in states:
+        closure.update(epsilon_closure(state, transitions))
+
+    return closure
+
+def remove_unreachable_states(automate):
+    # Remove all inacessible state
+    initial_state = automate['initial_state']
+    transitions = automate['transitions']
+
+    reachable = reachable_states(initial_state, transitions)
+
+    new_transitions = [transition for transition in transitions if transition[0] in reachable and transition[2] in reachable]
+
+    automate['states'] = list(reachable)
+    automate['transitions'] = new_transitions
+
+def remove_epsilon_transitions(automate):
+    # Remove all epsilon transition
+    transitions = automate['transitions']
+
+    epsilon_transitions = [transition for transition in transitions if transition[1] == '']
+
+    while epsilon_transitions:
+        for epsilon_transition in epsilon_transitions:
+            from_state = epsilon_transition[0]
+            to_state = epsilon_transition[2]
+            epsilon_closure_set = epsilon_closure_of_set([to_state], transitions)
+
+            for state in epsilon_closure_set:
+                new_transition = [from_state, epsilon_transition[1], state]
+                if new_transition not in transitions:
+                    transitions.append(new_transition)
+
+            transitions.remove(epsilon_transition)
+
+        epsilon_transitions = [transition for transition in transitions if transition[1] == '']
 
 automate = {
-    'alphabet': ['a','b'],
-    'states' : ['0','1','2','3'],
-    'initial_state' : '0',
-    'final_states' : ['2','3'],
+    'alphabet': ['a', 'b'],
+    'states': ['0', '1', '2', '3'],
+    'initial_state': '0',
+    'final_states': ['2', '3'],
     'transitions': [
-        ['0','1','b'],
-        ['0','3','a'],
-        ['1','1','a'],
-        ['1','2','b'],
-        ['3','3','a']
+        ['0', '1', 'b'],
+        ['0', '3', 'a'],
+        ['1', '1', 'a'],
+        ['1', '2', 'b'],
+        ['3', '3', 'a']
     ]
 }
 
-states = automate['transitions'][0][1]
-print(states)
+remove_unreachable_states(automate)
+remove_epsilon_transitions(automate)
 
-def adjacent(state):
-    return state
+print("Automate émondé:")
+print(automate)
 
-def is_accessible(automate, state):
-
-    states = automate['states']
-    visited = []
-    initial_state = automate['initial_state']
-    visited.append(initial_state)
-
-    # While stack isn't empty
-    while visited != []:
-        #current state is the last state visited
-        current_state = visited.pop()
-
-        # if current state is the one visited the state is accessible
-        if current_state == state:
-            return True
-
-        # else we look for all the adjacent state of our current state
-        else :
-            adjacents = adjacent(state)
-            for adjacent in adjacents :
-                # if the adjacent state hasn't been visited we had it to the visited stack
-                if adjacent not in visited :
-                    visited.append(adjacent)
-
-    # state isn't accessible
-    return False
-
-def is_coaccessible(state):
-    return state
-
-def excise(automate):
-
-    inaccesible_states = []
-    coaccessible_states = []
-    states = automate['states']
-
-    for state in states:
-        if not is_accessible(state):
-            inaccesible_states.append(state)
-
-    for state in states:
-        if not is_coaccessible(state):
-            coaccessible_states.append(state)
-
-    for state in states :
-        if state in inaccesible_states or state in coaccessible_states :
-            return 1
