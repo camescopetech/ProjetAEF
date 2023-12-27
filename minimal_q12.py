2f9eb1666a4eb2f2ab7aa5561a8e04d4db56a956
Voxidris <124724093+Voxidris@users.noreply.github.com>
2023-12-27 17:03:52 +0100

    Create minimal_q12.py
    Traiter le cas des autos non deterministe tjs à faire

---
+++ minimal_q12.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import automaton_q1 as q1
+import automatons_tests as test
+
+
+
+def minimal_automaton(automaton) : #based on find_cycles
+ 
+    df = q1.get_good_type(automaton, 'dataFrame')
+    alphabet = list(df.columns)[:-2]
+    initial_state = df[df.initial_state == True].index[0]
+    stack = [(initial_state,[initial_state])] # initialize the stack. Every element of it is a tuple containing the current state and the list of previous states
+    final_state = list(df[df.final_states == True].index) 
+    usefull_states = {initial_state} # must kept inital state
+    maybe = []
+
+    while stack :
+        current_state = stack[0][0]
+        path = stack[0][1]
+        if current_state in final_state : #ending path : the states are usefull
+            usefull_states.update(set(path + [current_state]))
+  
+        for letter in alphabet : #look for new states to find other paths
+            next_state = q1.is_transition_valid(df, letter, current_state)
+            if  next_state : 
+                if next_state[0] not in path : #state not already in the current path
+                    # print(path + next_state)
+                    stack.append((next_state[0], path + next_state)) # supply stack with updated values
+                else : maybe.append((next_state[0],path)) # we are in a loop : the states are usefull if we get back to an usefull element without loop
+        stack.pop(0)
+   
+    # add element usefull only in loops
+    for el in maybe : 
+        if el[0] in usefull_states :
+            usefull_states.update(el[1])
+
+    df_good =  df[df.index.isin(usefull_states)]  #delete useless state
+    df_good.loc[:,alphabet] = df_good[alphabet].applymap(lambda x: q1.null_transition if x not in usefull_states else x)  #delete transitions to useless states  
+    return  df_good         
+
+
+# print(q1.get_good_type(test.auto3,'dataFrame'))
+# print(minimal_automaton(test.auto3))
