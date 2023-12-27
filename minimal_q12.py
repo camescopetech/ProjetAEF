081cf395f787fb392d63f6e0906254c5a3cc33e3
Voxidris <124724093+Voxidris@users.noreply.github.com>
2023-12-27 19:28:19 +0100

    Update minimal_q12.py
    devrait être tout bon mais peu être plus testé

--- minimal_q12.py
+++ minimal_q12.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import automaton_q1 as q1
 import automatons_tests as test
-
-
+import re
 
 def minimal_automaton(automaton) : #based on find_cycles
  
@@ -15,6 +14,7 @@ def minimal_automaton(automaton) : #based on find_cycles
     maybe = []
 
     while stack :
+        
         current_state = stack[0][0]
         path = stack[0][1]
         if current_state in final_state : #ending path : the states are usefull
@@ -23,21 +23,28 @@ def minimal_automaton(automaton) : #based on find_cycles
         for letter in alphabet : #look for new states to find other paths
             next_state = q1.is_transition_valid(df, letter, current_state)
             if  next_state : 
-                if next_state[0] not in path : #state not already in the current path
-                    # print(path + next_state)
-                    stack.append((next_state[0], path + next_state)) # supply stack with updated values
-                else : maybe.append((next_state[0],path)) # we are in a loop : the states are usefull if we get back to an usefull element without loop
+                for state in next_state :
+                    if state not in path : #state not already in the current path
+                        stack.append((state, path + [state])) # supply stack with updated values
+                    else : maybe.append((state,path)) # we are in a loop : the states are usefull if we get back to an usefull element without loop
         stack.pop(0)
    
     # add element usefull only in loops
     for el in maybe : 
         if el[0] in usefull_states :
             usefull_states.update(el[1])
+    
+    df_good =  df[df.index.isin(usefull_states)] #delete useless state (ok if determinist)
+   
+    for letter in alphabet : # for not determinist transitions
+        df_good.loc[df_good[letter].str.contains(', '),letter] = df_good[letter].apply(lambda x :  ', '.join([el for el in x.split(', ') if el in usefull_states]))
+    df_good = df_good.replace('',q1.null_transition)  # if not determinist transitions and all to useless states
 
-    df_good =  df[df.index.isin(usefull_states)]  #delete useless state
-    df_good.loc[:,alphabet] = df_good[alphabet].applymap(lambda x: q1.null_transition if x not in usefull_states else x)  #delete transitions to useless states  
+    #delete transitions to useless states while ignoring not determinist transitions
+    df_good.loc[:,alphabet] = df_good[alphabet].applymap(lambda x: x if re.search(',',x) else q1.null_transition if x not in usefull_states  else x) 
+
     return  df_good         
 
 
-# print(q1.get_good_type(test.auto3,'dataFrame'))
-# print(minimal_automaton(test.auto3))
+# print(q1.get_good_type(test.auto11,'dataFrame'))
+# print(minimal_automaton(test.auto11))
